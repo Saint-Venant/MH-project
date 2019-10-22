bcbb7d954164ff8c5e3e3dea107c7713653f4419
Matt <matthieu.roux92@gmail.com>
2019-10-22 16:09:36 +0200

    constraints.py : add a function to check the constraints

--- constraints.py
+++ constraints.py
@@ -52,3 +52,13 @@ def contrainteCom(solution, Acom, NeighCom):
                 file.append(j)
 
     return violationCom
+
+def checkConstraints(solution, Acapt, Acom, NeighCom):
+    '''
+    For a given solution, checks whether or not the constraints are respected
+    '''
+    violationCapt = constraints.contrainteCapt(solution, Acapt)
+    violationCom = constraints.contrainteCom(solution, Acom, NeighCom)
+    sCapt = np.sum(violationCapt)
+    sCom = np.sum(violationCom)
+    return (sCapt == 0) and (sCom == 0)
