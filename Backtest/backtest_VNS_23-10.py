ecdf2e657dfeec194ecbdac05d9ecddffe850482
Matt <matthieu.roux92@gmail.com>
2019-10-23 01:22:46 +0200

    Backtest for VNS ready to be run on 23-10

---
+++ Backtest/backtest_VNS_23-10.py
@@ -0,0 +1,42 @@
+import sys
+sys.path.append('..//')
+
+import numpy as np
+import pickle as pkl
+import time
+
+import VNS
+
+
+
+names = [
+    'captANOR225_9_20.dat',
+    'captANOR400_10_80.dat',
+    'captANOR625_15_100.dat',
+    'captANOR900_15_20.dat',
+    'captANOR1500_15_100.dat',
+    'captANOR1500_21_500.dat']
+instanceNames = ['../Instances/{}'.format(x) for x in names]
+
+
+vectR = [(1,1), (1,2), (2,2), (2,3)]
+
+vectResult = []
+
+for instanceName in instanceNames:
+    for (Rcapt, Rcom) in vectR:
+        t1 = time.time()
+        solution, score = VNS.VNS(instanceName, Rcapt, Rcom)
+        t2 = time.time()
+        result = [instanceName, Rcapt, Rcom, solution, score, t2-t1]
+        vectResult.append(result)
+
+        print('instanceName : {}'.format(instanceName))
+        print('Rcapt : {} ; Rcom : {}'.format(Rcapt, Rcom))
+        print('  > score = {}'.format(score))
+        print()
+
+
+
+with open('backtest_recuit_basique.pkl', 'wb') as f:
+    pkl.dump(vectResult, f)
