86c8dff0bdcc9dd0591796ac08a744d8597d5968
Matt <matthieu.roux92@gmail.com>
2019-10-31 18:20:04 +0100

    .

---
+++ Backtest/backtest_VNS_28-10.py
@@ -0,0 +1,43 @@
+import sys
+sys.path.append('..//')
+
+import numpy as np
+import pickle as pkl
+import time
+
+from meta_VNS import VNS
+
+
+
+names = [
+    'captANOR225_9_20.dat',
+    'captANOR400_10_80.dat',
+    'captANOR625_15_100.dat',
+    'captANOR900_15_20.dat',
+    'captANOR1500_15_100.dat',
+    'captANOR1500_21_500.dat']
+instanceNames = ['../Instances/{}'.format(x) for x in names]
+
+
+vectR = [(1,1), (1,2), (2,2), (2,3)]
+
+if __name__ == '__main__':
+
+    vectResult = []
+
+    for instanceName in instanceNames:
+        for (Rcapt, Rcom) in vectR:
+            t1 = time.time()
+            solution, score = VNS.VNS(instanceName, Rcapt, Rcom, dtMax=60*6)
+            t2 = time.time()
+            result = [instanceName, Rcapt, Rcom, solution, score, t2-t1]
+            vectResult.append(result)
+
+            print('instanceName : {}'.format(instanceName))
+            print('Rcapt : {} ; Rcom : {}'.format(Rcapt, Rcom))
+            print('  > score = {}'.format(score))
+            print('  > dt = {}'.format(t2 - t1))
+            print()
+
+    with open('backtest_VNS_28-10.pkl', 'wb') as f:
+        pkl.dump(vectResult, f)
